d42abc08b5c4a42412d4b3221e8eb6ae6868879f
CarlosTeixeira615 <carlosteixeira.tc@hotmail.com>
2020-08-21 20:49:42 -0300

    ✨ finish project

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -60,6 +60,7 @@ export const TableContainer = styled.section`
     tr {
       display: grid;
       grid-template-columns: 1fr 1fr 1fr 1fr;
+      margin-top: 2px;
     }
     th {
       align-items: center;
